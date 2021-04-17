fc10beb9a000a05083159333e338d28dbccfb18f
Anh Viet <anhviet5121pc@gmail.com>
2021-04-18 05:02:32 +0700

    Change version of react-navigation

--- App.js
+++ App.js
@@ -1,28 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-import LoginScreen from "./src/screens/LoginScreen"
-import InfoScreen from "./src/screens/InfoScreen"
-import PersonalScreen from "./src/screens/PersonalScreen"
-import TeamScreen from "./src/screens/TeamScreen"
-import SignupScreen from "./src/screens/SignupScreen"
+import LoginScreen from "./src/screens/LoginScreen";
+import InfoScreen from "./src/screens/InfoScreen";
+import PersonalScreen from "./src/screens/PersonalScreen";
+import TeamScreen from "./src/screens/TeamScreen";
+import SignupScreen from "./src/screens/SignupScreen";
 
-const Stack = createStackNavigator();
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Login: LoginScreen,
+  }),
+  mainFlow: createBottomTabNavigator({
+    Personal: PersonalScreen,
+    Team: TeamScreen,
+    Info: InfoScreen,
+  }),
+});
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Info" component={InfoScreen} />
-        <Stack.Screen name="Personal" component={PersonalScreen} />
-        <Stack.Screen name="Team" component={TeamScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
-
-export default App;
+export default createAppContainer(switchNavigator);
